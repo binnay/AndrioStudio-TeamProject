57e152e95fc7f03fa0aeab546c88b98e679e7337
DaBeenBaek <2071167@hansung.ac.kr>
2023-11-24 03:27:44 +0900

    로그인 전 기능 사용 불가능하도록

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobileproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val transaction: FragmentTransaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.frameLayout, fragmentHome).commit()
+        transaction.replace(R.id.frameLayout, fragmentBLogin).commit()
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
 
@@ -28,12 +29,23 @@ class MainActivity : AppCompatActivity() {
 
             when (menuItem.itemId) {
                 R.id.homeItem -> {
-                    // 홈 아이템 클릭 시 다른 Fragment로 교체하도록 처리
-                     transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss()
+                    if(isLoggedIn) { // 로그인후에만 볼 수 있도록
+                        // 홈 아이템 클릭 시 다른 Fragment로 교체하도록 처리
+                        transaction.replace(R.id.frameLayout, fragmentHome)
+                            .commitAllowingStateLoss()
+                    } else {
+                        transaction.replace(R.id.frameLayout, fragmentBLogin)
+                        Toast.makeText(this, "로그인 후 사용하실 수 있습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }
                 R.id.chatItem -> {
-                    // 대화하기 아이템 클릭 시 다른 Fragment로 교체하도록 처리
-                    // transaction.replace(R.id.frameLayout, 다른 Fragment).commitAllowingStateLoss()
+                    if(isLoggedIn) {
+                        // 대화하기 아이템 클릭 시 다른 Fragment로 교체하도록 처리
+                        // transaction.replace(R.id.frameLayout, 다른 Fragment).commitAllowingStateLoss()
+                    } else {
+                        transaction.replace(R.id.frameLayout, fragmentBLogin)
+                        Toast.makeText(this, "로그인 후 사용하실 수 있습니다.", Toast.LENGTH_SHORT).show()
+                    }
                 }
                 R.id.mypageItem -> {
                     // '나의정보' 아이템 클릭 시 FragmentBLogin 또는 FragmentALogin을 표시
